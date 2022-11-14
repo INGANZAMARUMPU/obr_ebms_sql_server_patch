0fcf7f03395ff782ff80d7e92bac1b5b59ccb8f5
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-14 17:33:12 +0200

    facture request done

--- main.py
+++ main.py
@@ -11,7 +11,23 @@ conn = pyodbc.connect(
 )
 
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM Facture WHERE DateFact >= '2019-30-04 00:00:00'")
+
+query = """
+    SELECT * FROM
+        Facture
+    JOIN
+        clients
+    ON 
+        Facture.idCli = clients.IdCli
+    JOIN
+        adresseCli
+    ON
+        Facture.IdCli = adresseCli.IdCli
+    WHERE
+        DateFact >= '2019-30-04 00:00:00'
+"""
+
+cursor.execute(query)
 
 for i in cursor:
     print(i)
