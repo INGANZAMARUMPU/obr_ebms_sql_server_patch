912deb5d2d91757357df32ac2e05b5aa3b6086f5
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-22 15:50:55 +0200

    only select facture with signature

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     cursor = conn.cursor()
 
     with open("LAST.DAT", 'r') as file:
-        min_date = file.readline() or "2018-16-11 00:00:00"
+        min_date = file.readline() or "2022-16-11 00:00:00"
         print(f"NEW PROCESS STARTING ON {min_date}")
 
     query = """
@@ -69,6 +69,8 @@ def main():
             Facture.IdCli = adresseCli.IdCli
         WHERE
             Facture.DateFact > ?
+        AND
+            Facture.signatureobr IS NOT NULL
         ORDER BY
             Facture.DateFact
     """
